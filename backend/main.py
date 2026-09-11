faf80ed04c68295eb482f5e2aec515908886aefc
Lina Castro <lina.maria.c12345@hotmail.com>
2026-09-11 09:22:25 -0400

    Add GET task endpoint

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+tasks = []
+
 @app.get("/")
 def home():
     return {"message": "Cloud Tasks API is running!"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
